9d0fa444bd11232eac74c54025a4fe6d85cf6e9a
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2016-11-01 14:08:24 +0100

    Singleton example implemented: Czech Republic President

---
+++ DesignPatternsBasics/Singleton/CzechPresident.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton
+{
+    class CzechPresident
+    {
+        private static CzechPresident _instance;
+
+        public static CzechPresident GetCurrentPresident()
+        {
+            if (_instance == null)
+            {
+                _instance = new CzechPresident();
+                _instance.ElectionTime = DateTime.Now;
+            }
+
+            return _instance;
+        }
+
+        private DateTime ElectionTime { get; set; }
+
+        public string IntroduceYourself() =>
+$@"I am President of the Czech Republic, Milos Zelman!
+I am holding office for {(DateTime.Now - ElectionTime).TotalSeconds.ToString("#.##")} seconds.";
+    }
+}
---
+++ DesignPatternsBasics/Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First president:");
+            var president = CzechPresident.GetCurrentPresident();
+            Thread.Sleep(1500);
+            Console.WriteLine(president.IntroduceYourself());
+
+            Console.WriteLine("\nSecond president:");
+            Thread.Sleep(2500);
+            var otherPresident = CzechPresident.GetCurrentPresident();
+            Console.WriteLine(otherPresident.IntroduceYourself());
+
+            Console.WriteLine("\nAre they the same?");
+            Console.WriteLine(president.Equals(otherPresident));
+
+            Console.ReadKey();
+        }
+    }
+}
